8579767597455ab210e9531202d955131f560f04
Perera Dev <nashi.codes@gmail.com>
2024-11-15 21:16:51 -0300

    🚧 Refactoring Orders tests

--- back-end/__tests__/unit/models/orders.test.ts
+++ back-end/__tests__/unit/models/orders.test.ts
@@ -1,6 +1,7 @@
 import {Context, createMockContext, MockContext} from "../../../src/prisma/context";
 import {PrismaClient} from "@prisma/client";
 import req from "supertest";
+import server from "../../../src/server";
 
 let mockCtx: MockContext
 let ctx: Context
@@ -14,25 +15,58 @@ const db = new PrismaClient()
 test('[POST] /api/orders/', async () => {
     const order = {
         userId: await getRandomUserId(),
-        orderItem: [
+        OrderItem: [
             {
-                id: await getRandomProductId(),
+                productId: await getRandomProductId(),
                 quantity: Math.floor(Math.random() * 10)
             },
             {
-                id: await getRandomProductId(),
+                productId: await getRandomProductId(),
                 quantity: Math.floor(Math.random() * 10)
             },
             {
-                id: await getRandomProductId(),
+                productId: await getRandomProductId(),
                 quantity: Math.floor(Math.random() * 10)
             }
         ]
     }
 
-    let res = await req("localhost:5000").post('/api/orders').send(order)
+    let res = await req(server).post('/api/orders').send(order)
     expect(res.status).toBe(201)
     expect(res.body.message).toBe('Order created successfully')
+
+    order.userId = ""
+    res = await req(server).post('/api/orders').send(order)
+    expect(res.status).toBe(400)
+    expect(res.body.message).toBe('userId is a required field')
+
+    order.userId = await getRandomUserId()
+    order.OrderItem = []
+    res = await req("localhost:5000").post('/api/orders').send(order)
+    expect(res.status).toBe(400)
+    expect(res.body.message).toBe('OrderItem is a required field')
+
+    order.OrderItem = [
+        {
+            productId: "",
+            quantity: Math.floor(Math.random() * 10)
+        }
+    ]
+    res = await req("localhost:5000").post('/api/orders').send(order)
+    expect(res.status).toBe(400)
+    expect(res.body.message).toBe('productId is a required field')
+
+    order.OrderItem[0].productId = await getRandomProductId()
+    order.OrderItem[0].quantity = 0
+    res = await req("localhost:5000").post('/api/orders').send(order)
+    expect(res.status).toBe(400)
+    expect(res.body.message).toBe('quantity must be greater than or equal to 1')
+
+    order.OrderItem[0].quantity = Math.floor(Math.random() * 10)
+    res = await req("localhost:5000").post('/api/orders').send(order)
+    expect(res.status).toBe(201)
+    expect(res.body.message).toBe('Order created successfully')
+
 })
 
 
