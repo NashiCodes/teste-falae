016e0fd75492c0ed76c4d9217660bc4f8e5d8fbc
Perera Dev <nashi.codes@gmail.com>
2024-11-19 00:33:09 -0300

    💄 Add ThemeProvider

--- front-end/src/main.tsx
+++ front-end/src/main.tsx
@@ -1,10 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import {StrictMode} from 'react'
+import {createRoot} from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import Home from "@/routes/Home.tsx";
+import {ThemeProvider} from "@/components/theme-provider.tsx";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App/>,
+    },
+    {
+        path: "/home",
+        element: <Home/>
+    }
+]);
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+        <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+            <RouterProvider router={router}/>
+        </ThemeProvider>
+        {/*<App/>*/}
+    </StrictMode>,
 )
